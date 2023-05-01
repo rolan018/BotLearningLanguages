7ee9aca358f764035f574845d99d01bf36e77b50
rolan018 <dadashev.rr@phystech.edu>
2023-05-01 15:01:46 +0300

    change secret key

--- tga/tga/settings.py
+++ tga/tga/settings.py
@@ -27,7 +27,7 @@ BOT_TOKEN = str(os.getenv("SECRET_KEY"))
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "******"
+SECRET_KEY = str(os.getenv("SECRET_KEY_DJANGO"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
